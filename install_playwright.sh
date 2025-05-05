067a0f8c80165755774165580ff64ef87d457b3e
klebertanide <klebertanide@gmail.com>
2025-05-05 07:24:25 -0300

    Update install_playwright.sh

--- install_playwright.sh
+++ install_playwright.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-# instala dependências Python do projeto…
 pip install -r requirements.txt
-
-# baixa **só** o Chromium que o Playwright usa
 python -m playwright install chromium
