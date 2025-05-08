e0e4372349f75e103f6403d5c03b01d0a5dc3bf1
klebertanide <klebertanide@gmail.com>
2025-05-08 05:18:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -178,4 +178,4 @@ def download_zip(filename):
     return send_from_directory(ZIP_DIR, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
