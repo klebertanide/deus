f7bd06ab413e0f19a4f0a944a0ac61f5a54bd566
klebertanide <klebertanide@gmail.com>
2025-05-18 19:44:38 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -149,3 +149,7 @@ def transcrever():
     finally:
         try: fobj.close()
         except: pass
+            
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
