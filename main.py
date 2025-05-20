ba75c563b53e2c28cb0c26e361dab295eadb563d
klebertanide <klebertanide@gmail.com>
2025-05-20 16:06:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -139,3 +139,8 @@ def falar():
         slug=slug,
         drive_folder_url=f"https://drive.google.com/drive/folders/{subfolder_id}"
     )
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
